a8120fcc8357d696f484f3cba35ea2c6d54cc2de
Axel Demeyere <axel.demeyere59@gmail.com>
2025-08-20 16:14:24 +0200

    Ajout de la sécurité avec authentification Google via OAuth 2

---
+++ exo4/src/main/java/org/app/exo4/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.app.exo4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
